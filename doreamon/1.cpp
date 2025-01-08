4923b5f8a3f92cbc5923033887eb2ae1102ee0ce
Archana756 <archanakumarkhaniya@gmail.com>
2025-01-08 17:27:26 +0530

    Doraemon added

---
+++ doreamon/1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class gadget{
+
+    private:
+    int usecount = 0;
+
+    public:
+    string name;
+    int powerlevel;
+
+    void display(string a, int b){
+
+        name=a;
+        powerlevel=b;
+
+        cout<<"Gadget name : "<< name <<"    "<<"Powerlevel : "<<powerlevel<<endl;
+
+    }
+
+    void update()
+    {
+        usecount ++;
+
+        if (usecount > 10)
+        {
+            cout << "You are overusing gadgets !" << endl;
+        }
+        
+    }
+
+};
+
+int main()
+{
+    int use;
+    gadget nobita,doreamon;
+
+    nobita.display("Anywhere door", 20);
+    nobita.display("Bamboo Copter", 30);
+    nobita.display("Time Machine", 50);
+    nobita.display("Big light", 70);
+    nobita.display("4D Pocket", 90);
+
+    cout<<"Enter the value : ";
+    cin >> use;
+
+    for (int i = 1; i <= use; i++)
+    {
+        cout << "using gadget for " << i << " Time" << endl;
+
+        doreamon.update();
+    }
+        
+}
+
